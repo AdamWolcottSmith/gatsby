8fb2bc24b7eaa1bbec6ce61f8fa88c372ad19bdd
Wolcott <wolcott@Wolcotts-MacBook-Pro.local>
2024-03-31 14:24:41 -0400

    pages for portfolio site

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>Reach me here:</p>
+    </div>
+  );
+};
+
+export default ContactPage;
