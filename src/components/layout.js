110efa85c33ab0b57cc2e466c32ee472ce190328
Wolcott <adamwolcottsmith@gmail.com>
2024-03-31 15:06:44 -0400

    imported sass plugin and created style sheet

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,9 @@
 import React from "react";
-// import { Link } from "gatsby";
+
 import Footer from "../components/footer";
 import NavBar from "../components/navbar";
+
+import "../styles/index.scss";
 
 const IndexPage = (props) => {
   return (
